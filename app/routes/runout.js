a73119fb6f918e1748d6663d6622303cdd35e1a9
lucaforgia <luca_forgia@yahoo.it>
2019-12-18 15:56:55 +0100

    fatto esaurimenti

--- app/routes/runout.js
+++ app/routes/runout.js
@@ -2,7 +2,9 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model(){
-    return this.store.query('contraption',{availableQt:0});
+
+
+    return this.store.query('contraption',{filter:'runout'});
     // return this.store.findAll('running-out-contraptions');
   }
 });
