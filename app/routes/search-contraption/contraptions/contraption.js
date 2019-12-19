42c8c7c98d35c12374cdbbd031f8f8a0c296f488
lucaforgia <luca_forgia@yahoo.it>
2019-12-19 09:22:42 +0100

    change order status goes back

--- app/routes/search-contraption/contraptions/contraption.js
+++ app/routes/search-contraption/contraptions/contraption.js
@@ -8,10 +8,14 @@ export default Route.extend({
     return this.store.peekRecord('contraption', params.contraption_id);
   },
   actions:{
-    changeStatus(val){
+    changeStatus(val, getBack){
       this.orderApi.send(this.currentModel.get('id'), val).then((resp) => {
         let order_status = resp.data.attributes.order_status;
         this.currentModel.set('order_status', order_status);
+        // this.transitionTo('search-contraption.contraptions');
+        if(getBack){
+          this.send('back');
+        }
       },
         (error) =>{
           alert('error')
