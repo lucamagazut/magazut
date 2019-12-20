677e39e8f2f208930ba5d4ce47c98c4710695f84
lucaforgia <luca_forgia@yahoo.it>
2019-12-20 14:38:00 +0100

    category invece di search-filter

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,7 +2,7 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   beforeModel(){
-    this.store.findAll('search-filters');
+    this.store.findAll('category');
     this.store.findAll('operator');
     // this.replaceWith('home');todo
   },
