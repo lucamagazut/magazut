3f418287a7c3491c17b917bb48378ec91090d36a
lucaforgia <luca_forgia@yahoo.it>
2020-01-14 13:08:15 +0100

    start history

---
+++ app/models/unloading-history.js
@@ -0,0 +1,15 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  transaction_time: DS.attr('date'),
+  log: DS.attr(),
+  http_app_location: DS.attr(),
+  http_api_location: DS.attr(),
+  involved_quantity: DS.attr(),
+
+  employee_name: DS.attr(),
+  employee_second_name: DS.attr(),
+
+  contraption_denomination: DS.attr(),
+  contraption_id_code: DS.attr()
+});
--- app/routes/application.js
+++ app/routes/application.js
@@ -4,7 +4,6 @@ export default Route.extend({
   beforeModel(){
     this.store.findAll('category');
     this.store.findAll('operator');
-    // this.replaceWith('home');//todo
   },
   actions:{
     back(){window.history.back();}
