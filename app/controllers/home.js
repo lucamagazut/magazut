6bb7c1d48b99d012df3aacbe8d46f0cd4d34fa5e
lucaforgia <luca_forgia@yahoo.it>
2020-02-12 14:46:43 +0100

    from home controller to route

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -5,25 +5,5 @@ import EmberObject, {
 } from '@ember/object';
 
 export default Controller.extend({
-  history_page:0,
-  history_list: computed('history_page',function(){
-    return this.store.query('unloading-history',{'page':this.history_page})
-  }),
 
-  history_entries:computed('history_list',function(){
-    return this.history_list.get('length');
-  }),
-  actions:{
-    on_history_next(){
-      let page = this.history_entries === 15 ? this.history_page + 1 : this.history_page;
-      this.set('history_page', page);
-    },
-    on_history_prev(){
-      let page = this.history_page > 0 ? this.history_page - 1 : 0;
-      this.set('history_page', page);
-    },
-    onClickHistory(currentModelId){
-      this.transitionToRoute('search-contraption.contraptions',{queryParams: { text: `{c_id=${currentModelId}}`}});
-    }
-  }
 });
--- app/routes/home.js
+++ app/routes/home.js
@@ -1,5 +1,46 @@
 import Route from '@ember/routing/route';
+import EmberObject, {
+  observer,
+  computed
+} from '@ember/object';
 
 export default Route.extend({
+  history_list:[],
+  history_page:0,
+  history_entries:computed('history_list',function(){
+    return this.history_list.get('length');
+  }),
+
+  setupController: function(controller, model) {
+    controller.set('history_list',this.history_list);
+    controller.set('history_page',this.history_page);
+    controller.set('history_entries',this.history_entries);
+    this._super(controller, model);
+  },
+
+  setHistiryList(){
+    this.set('history_list',this.store.query('unloading-history',{'page':this.history_page}));
+  },
+  history_list_obs: observer('history_page',function(){
+    this.setHistiryList();
+  }),
+
+  beforeModel(){
+    this.setHistiryList();
+  },
+
+  actions:{
+    on_history_next(){
+      let page = this.history_entries === 15 ? this.history_page + 1 : this.history_page;
+      this.set('history_page', page);
+    },
+    on_history_prev(){
+      let page = this.history_page > 0 ? this.history_page - 1 : 0;
+      this.set('history_page', page);
+    },
+    onClickHistory(currentModelId){
+      this.transitionTo('search-contraption.contraptions',{queryParams: { text: `{c_id=${currentModelId}}`}});
+    }
+  }
 
 });
